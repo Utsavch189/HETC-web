6c735029dc5006aef3538bee92a240cb67c3a44f
Utsav <utsavpokemon9000chatterjee@gmail.com>
2022-04-10 08:48:59 +0530

    auth system

--- scholarship/views.py
+++ scholarship/views.py
@@ -8,6 +8,9 @@ from datetime import datetime
 from .models import *
 from django.core.mail import send_mail
 from .userid import ID
+from django.contrib.auth.decorators import login_required
+import random
+
 
 def home(request):
     return render(request,'home.html')
@@ -40,9 +43,12 @@ def register(request):
         date_of_birth2=str(date)+str(month)+str(year)
         smodel=Student(first_name=fname,last_name=lname,date_of_birth=date_of_birth1,gurdian_name=gurdian,contact=phone,whatsapp=whatsapp,email=email,address=address,school_college_name=instu,appearing_passed_12=status,board_name=board,appeared_wbjee_jeeMain=entrance,created_at=datetime.now())
         smodel.save()
-        userid=ID(fname)
+
+        a=random.randint(199,12999)
+        userid=fname+str(a)
+        
         subject='Thank You for registration'
-        body=f'your user id is {userid} and your password is {date_of_birth2}'
+        body=f'your user name is {userid} and your password is {date_of_birth2}'
         send_mail(
     subject,
     body,
@@ -50,14 +56,27 @@ def register(request):
     [email],
     fail_silently=False,
 )
+    
+        user = User.objects.create_user(userid, email, date_of_birth2)
+        user.save()
+    
     return render(request,'register.html')
 
 
 def Login(request):
-    return render(request,'login.html')
+   if request.method=="POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        print(username,password)
+        user=authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            return render(request,'exam.html')
+        else:
+            return render(request,'login.html')
 
-
+@login_required(login_url='login')
 def exam(request):
     if User.is_anonymous:
-        return redirect('/login')
+        return render(request,'login.html')
     return render(request,'exam.html')
